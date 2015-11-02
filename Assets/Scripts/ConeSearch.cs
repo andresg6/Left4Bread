7e39d76ce570da81d0b397b96b5c597762bfb5e5
andresg6 <andresg6@uci.edu>
2015-11-02 15:36:49 -0800

    Barebones code for ConeSearch started

--- Assets/Scripts/ConeSearch.cs
+++ Assets/Scripts/ConeSearch.cs
@@ -1,15 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
 
 public class ConeSearch : MonoBehaviour {
 
+    public float range;
+    public float angle;
+    public bool seeTarget = false;
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate ()
+    {
+        Collider2D[] PlayersNearMe = Physics2D.OverlapCircleAll(transform.position, range).Where(c => c.tag == "Player").ToArray();
+        if (PlayersNearMe.GetLength(0) > 0)
+        {
+            seeTarget = true;
+        }
+        else
+        {
+            seeTarget = false;
+        }
+
 	}
 }
