9768acf3487ce26ad4c32d7308b1b6e6eabe350e
Ian Tokuyama <igolftokuyama@gmail.com>
2015-11-09 13:48:58 -0800

    Updating
    simple updates to enemy

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+    float speed = 1.0f;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        Movement();	
+	}
+
+    void Movement()
+    {
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector2.down * speed * Time.deltaTime);
+        }
+    }
+}
