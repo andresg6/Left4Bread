3ad573acad95656b42aaa569deea2ed504f0b034
Andrew <aschau@uci.edu>
2015-11-28 21:58:18 -0800

    Added pickups
    Pickups disappear on collision

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickup : MonoBehaviour
+{
+
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
