632a4ce56f3a5bcd22684f8136b14a7cf1386c99
Andrew <aschau@uci.edu>
2015-11-28 23:10:02 -0800

    added enemy in AndrewTest scene
    Enemy follows you upon detection inside radius

---
+++ Assets/Scripts/BasicDetect.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicDetect : MonoBehaviour {
+
+    public bool detected = false;
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Renderer renderer = GetComponent<Renderer>();
+            detected = true;
+            renderer.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/BasicEnemy.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicEnemy : MonoBehaviour
+{
+
+    float speed = 10.0f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        BasicDetect detection = GameObject.Find("Enemy Vision 1").GetComponent<BasicDetect>();
+        if (detection.detected)
+        {
+            Transform player = GameObject.FindWithTag("Player").transform;
+            Movement(player);
+        }
+    }
+
+    void Movement(Transform player)
+    {
+        if (Vector2.Distance(transform.position, player.position) > 1f)
+        {
+            //transform.Translate(Vector2.left * speed * Time.deltaTime);
+            
+            //if (Time.deltaTime == 3)
+            //{
+            //    transform.Translate(Vector2.right * speed * 3);
+            //}
+
+            //transform.Translate(Vector2.MoveTowards(transform.position, player.position, 1));
+            transform.position = Vector3.MoveTowards(new Vector3(transform.position.x, transform.position.y, transform.position.z), player.position, speed * Time.deltaTime);
+        }
+    }
+}
