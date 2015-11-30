db444ddcebd95cc21805d3360307d76bf6a60051
Andrew <aschau@uci.edu>
2015-11-29 16:17:09 -0800

    Added LightEnemy
    Changed BasicEnemy to LightEnemy based on use of light object.

---
+++ Assets/Standard Assets/Characters/AndrewEnemy/Scripts/LightDetect.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine;
+using System.Collections;
+
+public class LightDetect : MonoBehaviour
+{
+
+    public bool detected = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (detected)
+        {
+            Collider collider = GetComponent<Collider>();
+            collider.enabled = false;
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            detected = true;
+        }
+    }
+}
+
