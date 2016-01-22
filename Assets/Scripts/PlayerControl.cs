4af466b38a471f4a0304b3d51029c4d7959f571d
Andrew <aschau@uci.edu>
2016-01-22 13:51:45 -0800

    got a basic sound detection working.

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
 
 public class PlayerControl : MonoBehaviour {
 
     public float speed;
+    public float range;
+    public bool loud = false;
 
 
         //void Start()
@@ -11,8 +14,19 @@ public class PlayerControl : MonoBehaviour {
         //    Physics2D.IgnoreLayerCollision(0, 1, true);
         //}
 	// Update is called once per frame
-	void Update ()
+	void FixedUpdate ()
     {
+        Collider2D[] enemiesnearby = Physics2D.OverlapCircleAll(transform.position, range).Where(c => c.tag == "Enemies").ToArray();
+        if (loud)
+        {
+            foreach (Collider2D enemy in enemiesnearby)
+            {
+                Vector3 targetDir = transform.position - enemy.transform.position;
+                float angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg;
+                Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+                enemy.transform.rotation = Quaternion.Slerp(enemy.transform.rotation, q, Time.deltaTime * speed);
+            }
+        }
         Movement();	
 	}
 
--- Assets/Standard Assets/Characters/LightEnemy/Scripts/ConeSearch2.cs
+++ Assets/Standard Assets/Characters/LightEnemy/Scripts/ConeSearch2.cs
@@ -5,10 +5,8 @@ using System.Linq;
 public class ConeSearch2 : MonoBehaviour
 {
     public float speed = 2.0f;
-    //public bool detected = false;
-    //public Transform range1, range2, range3;
+    public float range = 5.0f;
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         if (GameObject.FindWithTag("Player") != null)
@@ -21,7 +19,6 @@ public class ConeSearch2 : MonoBehaviour
                 {
                     if (l.detected)
                     {
-                        //Debug.DrawRay(transform.position, player.position - transform.position, Color.cyan, 20, true);
                         RaycastHit2D hit = Physics2D.Raycast(transform.position, player.position - transform.position, 99999, LayerMask.GetMask("Test"));
                         if (hit.collider.tag == "Player")
                         {
@@ -30,58 +27,6 @@ public class ConeSearch2 : MonoBehaviour
                     }
                 }
             }
-
-            //Light l = GetComponentInChildren<Light>();
-            //float range = l.range;
-            //float scope = l.spotAngle;
-
-            //Vector3 targetDir = player.position - transform.position;
-            //float angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg;
-
-            //Vector3 t = transform.eulerAngles;
-            //float angle2 = Mathf.Atan2(t.y, t.x) * Mathf.Rad2Deg;
-
-            //Debug.Log("Player: " + angle);
-            //Debug.Log("ENEMY: " + transform.rotation.z * 180 + scope / 2);
-            //Debug.Log((((transform.rotation.z * 180) + scope) / 2) - angle);
-            //Debug.Log((((transform.rotation.z * 180) - scope) / 2) - angle);
-            //Debug.DrawRay(transform.position, range1.position, Color.cyan, 20, true);
-            //Debug.DrawLine(transform.position, range1.position, Color.cyan);
-
-            //float angle2 = transform.rotation.z * 180;
-            //if (Vector3.Distance(transform.position, player.position) <= range)
-            //{
-            //    if (angle <= angle2 + scope / 2 && angle >= angle2 - scope / 2)
-            //    {
-            //        //RaycastHit2D hit = Physics2D.Raycast(transform.position, player.position - transform.position);
-
-            //        Vector3 temp = range1.position;
-            //        for (int i = 0; i < 100; i++)
-            //        {
-            //            Debug.DrawLine(range2.position, temp);
-            //            RaycastHit2D hit = Physics2D.Linecast(range2.position, temp);
-
-            //            if (hit.collider != null)
-            //            {
-            //                if (hit.collider.tag == "Player")
-            //                {
-            //                    detected = true;
-            //                }
-            //            }
-
-            //            temp = new Vector3(temp.x, temp.y - .01f, temp.z);
-            //        }
-
-
-
-
-            //    }
-            //}
-
-            //else
-            //{
-            //    detected = false;
-            //}
     }
 
     void Movement(Transform player)
