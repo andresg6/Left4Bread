cd0be081ddc543d8ce921b9c00552aba58114812
andresg6 <andresg6@uci.edu>
2016-02-12 16:44:03 -0800

    Setup for proper adding/removing of health; Animation for health propery stops at Idle

--- Assets/Scripts/CanvasController.cs
+++ Assets/Scripts/CanvasController.cs
@@ -1,17 +1,20 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 public class CanvasController : MonoBehaviour {
     public Image healthImage;
     public RectTransform HPPanel;
-    private PlayerControl player;
-    private int hpMax;
-    private int currentHP; 
+
+    private int hpMax = 0;
+    private int currentHP = 0;
+    private Stack<Image> HPStack;
+    private float widthRatio = 50;
+    private float halfCenter;
 	
 	void Start () {
-        player = FindObjectOfType<PlayerControl>().GetComponent<PlayerControl>();
+        halfCenter = widthRatio / 2;
         hpMax = 3;
         currentHP = 3;
 	}
@@ -24,13 +27,32 @@ public class CanvasController : MonoBehaviour {
     private void updatePlayerMaxHealth(int newMax)
     {
         //resize health panel
-        int widthRatio = (int)HPPanel.sizeDelta.x / hpMax;
         hpMax = newMax;
         HPPanel.sizeDelta = new Vector2(newMax * widthRatio, HPPanel.sizeDelta.y);
     }
 
-    private void updatePlayerCurrentHealth(int currentHP)
+    private void updatePlayerCurrentHealth(int newHP)
     {
-        
+        if(newHP > currentHP) //if Healed health
+        {
+            int diff = newHP - currentHP + HPStack.Count;
+            currentHP = newHP;
+            for(int i = HPStack.Count; i < diff; i++) //Create new health UI Icons & add to health stack
+            {
+                Image newHPImage = Instantiate(healthImage) as Image;
+                newHPImage.rectTransform.anchoredPosition = new Vector2(halfCenter + (i - 1) * widthRatio, newHPImage.rectTransform.anchoredPosition.y);
+                HPStack.Push(newHPImage);
+            }
+        }
+        else if(newHP < currentHP) //else if Lost health
+        {
+            int diff = HPStack.Count - (currentHP - newHP);
+            currentHP = newHP;
+            for(int i = HPStack.Count; i > diff; i--) //Pop health icons from stack and run "lost" animation
+            {
+                Image lostHP = HPStack.Pop();
+
+            }
+        }   
     }
 }
---
+++ Assets/Scripts/UI/UI_HP.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_HP : MonoBehaviour {
+
+    private bool isDying;
+    private Animator anim;
+
+	void Start () {
+        isDying = true;
+        anim = GetComponent<Animator>();
+    }
+	
+	void Update () {
+	    if(isDying)
+        {
+            if (anim.GetCurrentAnimatorStateInfo(0).IsTag("Dead"))
+            {
+                Debug.Log("Kill me yogi");
+                Destroy(this);
+            }
+        }
+	}
+
+    void lostAnimation()
+    {
+        anim.SetTrigger("Dead");
+        isDying = true;
+    }
+}
