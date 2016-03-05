af1a32d8890e3afd258166be9e602786b935f68e
Andrew <aschau@uci.edu>
2016-03-04 17:44:53 -0800

    added attacking (animations still inefficient) and enemy invincibility

--- Assets/Scripts/conesearch2.cs
+++ Assets/Scripts/conesearch2.cs
@@ -4,8 +4,11 @@ using System.Linq;
 
 public class conesearch2 : Character
 {
-    public float speed = 2.0f;
+    public float speed = 5.0f;
     public bool alert = false;
+    public bool invincible = false;
+    public float timehit;
+    public float invincibleTime = 1;
     float alertPercentage = 0.0f;
     float alertStep = 10.0f;
 
@@ -43,6 +46,24 @@ public class conesearch2 : Character
                 Movement(player.transform);
             }
         }
+       
+        if (invincible)
+        {
+            StartCoroutine("Flasher");
+            if (timehit + invincibleTime < Time.realtimeSinceStartup)
+            {
+                invincible = false;
+                speed = 5.0f;
+            }
+        }
+    }
+
+    IEnumerator Flasher()
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0f);
+        yield return new WaitForSeconds(.1f);
+        this.gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
+        yield return new WaitForSeconds(.1f);
     }
 
     void Movement(Transform player)
