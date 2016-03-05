dc90163ac6bd491ee6c0b370e276de02e6c6076b
andresg6 <andresg6@uci.edu>
2016-03-04 17:38:14 -0800

    Begun work on scale canvas Added axis "Info" to Unity Input Manager

---
+++ Assets/ScaleCanvasController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScaleCanvasController : MonoBehaviour {
+
+    public Text TaskText;
+    public RectTransform TextPanel;
+    private bool isHidden;
+
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveScaledItems();
+	}
+
+    private void moveScaledItems()
+    {
+        if (!isHidden)
+        {
+            
+        }
+    }
+}
