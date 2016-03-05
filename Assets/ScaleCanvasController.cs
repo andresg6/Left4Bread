d4cb6ab94320fe0ec62c7faf338612ae7279891b
andresg6 <andresg6@uci.edu>
2016-03-04 18:04:39 -0800

    Tab now controls showing of extra info UI

--- Assets/ScaleCanvasController.cs
+++ Assets/ScaleCanvasController.cs
@@ -11,19 +11,38 @@ public class ScaleCanvasController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	    
+        isHidden = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        handleKeyPress();
         moveScaledItems();
 	}
+
+    private void handleKeyPress()
+    {
+        if (Input.GetButtonDown("Info"))
+        {
+            isHidden = false;
+        }
+        if (Input.GetButtonUp("Info"))
+        {
+            isHidden = true;
+        }
+    }
 
     private void moveScaledItems()
     {
         if (!isHidden)
         {
-            
+            TaskText.gameObject.SetActive(true);
+            TextPanel.gameObject.SetActive(true);
+        }
+        else
+        {
+            TaskText.gameObject.SetActive(false);
+            TextPanel.gameObject.SetActive(false);
         }
     }
 }
