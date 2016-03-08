d05bfeba58c8c5c75aefec958b045388f6db4377
andresg6 <andresg6@uci.edu>
2016-03-07 16:19:35 -0800

    Organzied UI Prefabs Made UICoordinator to tie 2 seperate UI's together

---
+++ Assets/Scripts/MainUIController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainUIController : MonoBehaviour {
+    private PixelCanvasController pcc;
+    private ScaleCanvasController scc;
+
+	// Use this for initialization
+	void Start () {
+        pcc = FindObjectOfType<PixelCanvasController>();
+        scc = FindObjectOfType<ScaleCanvasController>();
+	}
+
+    public void updatePlayerHealth(int remainingHealth, int maxHealth)
+    {
+        pcc.updatePlayerHealth(remainingHealth, maxHealth);
+    }
+}
