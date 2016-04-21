cb284754b6011d82f65f67754cf125c841afb65a
kinseyragequit <kinseylynaugh@gmail.com>
2016-04-21 14:46:34 -0700

    Merge remote-tracking branch 'refs/remotes/origin/master' into Kinsey made puddle script more consistent with inheritance structure

--- Assets/Scripts/Puddle.cs
+++ Assets/Scripts/Puddle.cs
@@ -3,7 +3,15 @@
 public class Puddle : Environment
 {
 
-    public void OnTriggerStay2D(Collider2D other)
+    public void Update()
+    {
+        if (isInteracting)
+        {
+            player.splashy = true;
+        }
+    }
+
+    /*public void OnTriggerStay2D(Collider2D other)
     {
 
         if (other.gameObject.tag == "Player" && other.gameObject.layer == LayerMask.NameToLayer("Test"))
@@ -12,5 +20,7 @@ public class Puddle : Environment
             player.splashy = true;
         }
 
-    }
+    }*/
+
+    
 }
